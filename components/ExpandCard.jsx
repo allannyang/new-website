49003d38869eaeacb4f41d1241dcf47554473c57
Allan Yang <102131569+allannyang@users.noreply.github.com>
2022-11-22 04:13:12 -0500

    mobile tweaks

--- components/ExpandCard.jsx
+++ components/ExpandCard.jsx
@@ -75,15 +75,15 @@ const ExpandCard = () => {
                 }}
                 classname={styles.expand}
               >
-                <LangCard description="Java ████████████"></LangCard>
+                <LangCard description="Java █████████"></LangCard>
                 <br />
-                <LangCard description="C █████████"></LangCard>
+                <LangCard description="C ███████"></LangCard>
                 <br />
-                <LangCard description="HTML/CSS/JS ████████"></LangCard>
+                <LangCard description="HTML/CSS/JS ██████"></LangCard>
                 <br />
-                <LangCard description="Python ██████"></LangCard>
+                <LangCard description="Python █████"></LangCard>
                 <br />
-                <LangCard description="SQL ████"></LangCard>
+                <LangCard description="SQL ███"></LangCard>
                 <br />
                 <LangCard description="Racket ████"></LangCard>
                 <br />
